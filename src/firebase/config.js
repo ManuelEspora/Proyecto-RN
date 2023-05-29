e76a3b580d336c979f846a2edabbeabce90f6729
ManuelEspora <mespora@udesa.edu.ar>
2023-05-29 10:51:46 -0300

    Creacion de ProyectoRN e instalacion de firebase y navigation

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbQPlorVNiXl6nzhYUWMi3kqfzadDv58c",
+  authDomain: "proyecto-rn-d5016.firebaseapp.com",
+  projectId: "proyecto-rn-d5016",
+  storageBucket: "proyecto-rn-d5016.appspot.com",
+  messagingSenderId: "964711170055",
+  appId: "1:964711170055:web:36423d4bf1fb245e1eaaa2"
+};
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const storage = app.storage();
+export const db = app.firestore();
+
